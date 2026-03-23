aabadb79d4df69db35567514d04b982b1e4b1cef
23DP1EMozg <A230240EM@rvt.lv>
2026-03-23 16:06:25 +0200

    Created start screen

--- app/src/main/java/com/spuldz/praksesprojekts/ui/screens/StartScreen.kt
+++ app/src/main/java/com/spuldz/praksesprojekts/ui/screens/StartScreen.kt
@@ -1,27 +1,61 @@
 package com.spuldz.praksesprojekts.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import com.spuldz.praksesprojekts.R
+import com.spuldz.praksesprojekts.ui.theme.BackgroundColor
+import com.spuldz.praksesprojekts.ui.theme.PrimaryColor
+import com.spuldz.praksesprojekts.ui.theme.TitleStyle
+import com.spuldz.praksesprojekts.ui.theme.sizing
 
 @Composable
 fun StartScreen() {
     Column(
         modifier = Modifier
-            .background(Color.Red)
+            .background(BackgroundColor)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("Welcome!")
-        Button(onClick = {}) {
+        Image(
+            modifier = Modifier
+                .width(sizing.dp100)
+                .height(sizing.dp100),
+            contentScale = ContentScale.Crop,
+            painter = painterResource(R.drawable.start_screen_icon),
+            contentDescription = null,
+        )
+        Text(
+            text = "Welcome to sudoku!",
+            style = TitleStyle
+
+        )
+        Button(
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(top = sizing.dp20),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = PrimaryColor,
+                contentColor = Color.White
+            ),
+            onClick = {}
+        ) {
             Text("Start")
         }
     }
---
+++ app/src/main/java/com/spuldz/praksesprojekts/ui/theme/Sizing.kt
@@ -0,0 +1,47 @@
+package com.spuldz.praksesprojekts.ui.theme
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class TailwindSizing(
+    val dp0: Dp = 0.dp,
+    val dp1: Dp = 1.dp,
+    val dp2: Dp = 2.dp,
+    val dp3: Dp = 3.dp,
+    val dp4: Dp = 4.dp,
+    val dp5: Dp = 5.dp,
+    val dp6: Dp = 6.dp,
+    val dp8: Dp = 8.dp,
+    val dp10: Dp = 10.dp,
+    val dp12: Dp = 12.dp,
+    val dp14: Dp = 14.dp,
+    val dp16: Dp = 16.dp,
+    val dp17: Dp = 17.dp,
+    val dp18: Dp = 18.dp,
+    val dp20: Dp = 20.dp,
+    val dp22: Dp = 22.dp,
+    val dp24: Dp = 24.dp,
+    val dp26: Dp = 26.dp,
+    val dp28: Dp = 28.dp,
+    val dp30: Dp = 30.dp,
+    val dp32: Dp = 32.dp,
+    val dp34: Dp = 34.dp,
+    val dp36: Dp = 36.dp,
+    val dp38: Dp = 38.dp,
+    val dp40: Dp = 40.dp,
+    val dp42: Dp = 42.dp,
+    val dp44: Dp = 44.dp,
+    val dp100: Dp = 100.dp
+)
+
+val LocalSizing = compositionLocalOf { TailwindSizing() }
+
+// We want this value to be on our theme, even if we do not use the theme object itself
+/** A local provider of the [TailwindSizing] design system values. */
+val sizing: TailwindSizing
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSizing.current
