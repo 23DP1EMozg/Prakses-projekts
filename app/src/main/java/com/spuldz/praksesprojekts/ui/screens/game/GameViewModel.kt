60adcc5c276e89281c1aa0f54d993c321bdcaaf7
23DP1EMozg <A230240EM@rvt.lv>
2026-04-01 12:06:40 +0300

    Merge conflict fix

--- app/src/main/java/com/spuldz/praksesprojekts/ui/screens/game/GameViewModel.kt
+++ app/src/main/java/com/spuldz/praksesprojekts/ui/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.spuldz.praksesprojekts.ui.screens.game
 
 import androidx.lifecycle.ViewModel
+import com.spuldz.praksesprojekts.core.common.launch
 import com.spuldz.praksesprojekts.core.models.GridCellModel
 import com.spuldz.praksesprojekts.core.repositories.GameRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
